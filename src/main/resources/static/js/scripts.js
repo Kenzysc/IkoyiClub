118c3fb47c4e5ee25cae435e500ac79a0b532386
Kenzysc <kenzysc@gmail.com>
2023-09-25 22:59:53 +0100

    feat: Add guest login button

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -5,3 +5,24 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  var guestLoginForm = document.getElementByCgetElementsByClassName("login-form");
+  var guestNameInput = document.getElementById("username");
+  var guestEmailInput = document.getElementById("password");
+  var guestLoginButton = document.getElementById("guestLoginButton");
+
+
+  function fillGuestInfo() {
+    guestNameInput.value = "kenneth";
+    guestEmailInput.value = "password";
+  }
+
+  
+  guestLoginButton.addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent the form from submitting by default
+    fillGuestInfo(); 
+    guestLoginForm.submit();
+  });
+});
